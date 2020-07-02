0a0ef8ddc160e5797e26fb60018dcb47ba7d1372
Ivan babuder <babacwow@gmail.com>
2020-07-02 19:57:03 +0200

    added menu item styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homepage.component';
+
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
@@ -8,6 +9,6 @@ function App() {
       <HomePage />
     </div>
   );
-}
+} 
 
 export default App;
