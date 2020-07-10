188946c1cc9e56857adccb19df12edaaaf4a463d
Ivan babuder <babacwow@gmail.com>
2020-07-10 19:42:33 +0200

    added collections overview component, added redux-persist,

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,29 +1,12 @@
 import React from 'react';
 
-import SHOP_DATA from './shop.data';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+const ShopPage = ({collections}) => (
 
-class ShopPage extends React.Component {
-    constructor(props){
-        super(props);
+         <div className='shop-page'>
+             <CollectionsOverview />
+        </div>
+);
 
-        this.state= {
-            collections: SHOP_DATA
-
-            
-        }
-    }
-
-    render(){
-        const {collections} = this.state;
-        return (<div className='shop-page'>
-            {collections.map(({id, ...otherCollectionProps}) =>(
-                <CollectionPreview key={id} {...otherCollectionProps} />
-            ))
-            }
-        </div>);
-    }
-} 
-
-export default ShopPage;
+export default ShopPage;    
